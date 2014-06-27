fa22f5eb5e27091b6717bafe028d8b233670e25a
grigit <ariasgri@gmail.com>
2014-06-26 23:58:57 -0300

    Mejorando los modelos con claves foraneas

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,4 @@ class Comment < ActiveRecord::Base
   
   belongs_to :post
   
-  #def post
-   # Post.find(post_id)
-  #end
-  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,6 @@ class Post < ActiveRecord::Base
 
   has_many :comments
   
-  #def comments
-  #  Comment.where({post_id: id})
-  #end
-  
   def ultimo
     comments.last
   end
